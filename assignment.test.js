c91fa39b4c21b8eec3a4829bb3a35d58e21453e5
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2017-10-19 02:03:43 +0000

    added tests for add() and one initial test for throws

--- assignment.test.js
+++ assignment.test.js
@@ -1,5 +1,25 @@
-describe('Remove me', () => {
-  test('Remove this', () => {
-    expect(1).toBe(1);
+import { add, throws } from "./index";
+
+describe('Testing the add() function', () => {
+  test('Adding 1 and 1, add(1, 1)', () => {
+    expect(add(1,1)).toBe(2);
+  });
+
+  test('Adding null and 1, add(null, 1)', () => {
+    expect(add(null,1)).toBe(1);
+  });
+
+  test('Empty call to function, add()', () => {
+    expect(add()).toBeNaN();
+  });
+
+  test('Adding a positive number and a negative number, add(1, -1)', () => {
+    expect(add(1,-1)).toBe(0);
+  });
+});
+
+describe('Testing that the throws function', () => {
+  test('throws an error', () => {
+    expect(throws).toThrowError();
   });
 });
